e217619cc55086cfdba6ae7dc26b13e7567d1fb9
Nishimwe Alain Bruce <nishalbruce@gmail.com>
2021-09-29 20:53:48 +0200

    eltanganyika

---
+++ php/menu_index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+     <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <!-- CSS only -->
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"/>
+          <link rel="stylesheet" href="../css/style.css">
+     </head>
+     <body>
+          <nav class="navbar navbar-light bg-light border-bottom" id="header">
+               <div class="container-fluid content-justify-center">
+                    <div></div>
+                    <div class="header">
+                         <a class="navbar-brand" href="../index.php">
+                              <img src="../img/elt logo.png" alt="Etoile du Lac Tanganyika" width="300" height="50">
+                         </a>
+                    </div>
+                    <div></div>
+               </div>
+          </nav>
+
+          <!-- start nav section -->
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+               <div class="container-fluid">
+               <button class="navbar-toggler text-light border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style="background-color: #00f;">
+                    <i class="fas fa-bars" style="color: #fff;font-size: 28px;"></i>
+               </button>
+               <div class="collapse navbar-collapse text-center" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                         <a class="nav-link active text-primary" aria-current="page" href="#">Accueil</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                         <a class="nav-link dropdown-toggle text-primary" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                         Rubriques
+                         </a>
+                         <ul class="dropdown-menu bg-light text-center" aria-labelledby="navbarDropdown">
+                         <li><a class="dropdown-item" href="#">Sport</a></li>
+                         <li><a class="dropdown-item" href="#">Musique</a></li>
+                         <li><a class="dropdown-item" href="#">Zoom Sur</a></li>
+                         <li><a class="dropdown-item" href="#">&Eacute;ducation</a></li>
+                         <li><a class="dropdown-item" href="#">Divertissement</a></li>
+                         <li><a class="dropdown-item" href="#">Environnement</a></li>
+                         </ul>
+                    </li>
+                    <li class="nav-item">
+                         <a class="nav-link text-primary" href="#">Qui sommes-nous</a>
+                    </li>
+                    </ul>
+               </div>
+               </div>
+               </nav>
+          <!--  end nav section -->
+
+
+
+          <!-- JavaScript Bundle with Popper -->
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+     </body>   
+</html>
